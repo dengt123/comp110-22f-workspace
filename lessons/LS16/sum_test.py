d7f7d837afcd1a2c1977fd3aa61f31b7fa071cf9
Timothy Deng <dengt@ad.unc.edu>
2022-09-19 03:30:13 -0400

    ls16

---
+++ lessons/LS16/sum_test.py
@@ -0,0 +1,17 @@
+from sum import sum
+
+
+def test_sum_empty() -> None:
+    assert sum([]) == 0.0
+
+
+def test_sum_single_item() -> None:
+    assert sum([110.0]) == 110.0
+
+
+def test_sum_many_items() -> None:
+    assert sum([1.0, 2.0, 3.0]) == 6.0
+
+
+def test_sum_many_items_again() -> None:
+    assert sum([-1.0, 1.0, -2.0, 2.0]) == 0.0
